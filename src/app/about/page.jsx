92e9a1a42403b054626820fe7f43b6ed775d5906
hammad1811 <hammadsaleem1811@gmai..com>
2025-06-04 19:43:27 +0500

    adding Link on the button about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,43 +1,48 @@
-import React from 'react'
-import HeroBanner from '@/components/HeroBanner'
-import InstagramGallery from '@/components/InstagramGallery'
-import { Button } from '@mui/material'
-import { GoArrowUpRight } from 'react-icons/go'
-import AboutUs from '@/components/AboutUs'
-import TopCategory from '@/components/TopCategory'
-import AboutCollection from '@/components/AboutCollection'
+import React from "react";
+import HeroBanner from "@/components/HeroBanner";
+import InstagramGallery from "@/components/InstagramGallery";
+import { Button } from "@mui/material";
+import { GoArrowUpRight } from "react-icons/go";
+import AboutUs from "@/components/AboutUs";
+import TopCategory from "@/components/TopCategory";
+import AboutCollection from "@/components/AboutCollection";
+import Link from "next/link";
 
 function page() {
   return (
     <>
-    <HeroBanner bannerName={"About"} />
-    <AboutUs/>
-    <AboutCollection/>
-    <TopCategory/>
-    <section className='py-12 flex justify-center items-center flex-col space-y-4'>
-        <h1 className='text-4xl text-black'>Ready to experience Luxury ?</h1>
-        <p className='text-[#868686]'>Look at the latest collection that we offer</p>
-        <Button
-                    sx={{
-                      backgroundColor: "#1D1D1D",
-                      width: "auto",
-                      height: "45px",
-                      borderRadius: "0px",
-                      border: "1px solid gray",
-                      "&:hover": {
-                        backgroundColor: "#333333",
-                      },
-                    }}
-                    variant="contained"
-                  >
-                    <p className="flex gap-2 items-center text-sm px-4">
-                      Contact Us <GoArrowUpRight />
-                    </p>
-                  </Button>
-    </section>
-    <InstagramGallery/>
+      <HeroBanner bannerName={"About"} />
+      <AboutUs />
+      <AboutCollection />
+      <TopCategory />
+      <section className="py-12 flex justify-center items-center flex-col space-y-4">
+        <h1 className="text-4xl text-black">Ready to experience Luxury ?</h1>
+        <p className="text-[#868686]">
+          Look at the latest collection that we offer
+        </p>
+        <Link href={"/contact"}>
+          <Button
+            sx={{
+              backgroundColor: "#1D1D1D",
+              width: "auto",
+              height: "45px",
+              borderRadius: "0px",
+              border: "1px solid gray",
+              "&:hover": {
+                backgroundColor: "#333333",
+              },
+            }}
+            variant="contained"
+          >
+            <p className="flex gap-2 items-center text-sm px-4">
+              Contact Us <GoArrowUpRight />
+            </p>
+          </Button>
+        </Link>
+      </section>
+      <InstagramGallery />
     </>
-  )
+  );
 }
 
-export default page
+export default page;
